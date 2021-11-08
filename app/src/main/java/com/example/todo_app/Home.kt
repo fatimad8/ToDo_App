627cff968a16be29aa377ed41dddc42dcdda3923
mac <aldagdougfatm@gmail.com>
2021-11-08 14:39:18 +0300

    add,delete

--- app/src/main/java/com/example/todo_app/Home.kt
+++ app/src/main/java/com/example/todo_app/Home.kt
@@ -7,20 +7,17 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.*
 import androidx.annotation.RequiresApi
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todo_app.model.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.time.LocalDate
 import java.util.*
-
-
 
 
 class Home : AppCompatActivity() {
@@ -31,47 +28,51 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         var mSearchView = findViewById<androidx.appcompat.widget.SearchView>(R.id.mSearchView)
         var mRecyclerView = findViewById<RecyclerView>(R.id.mRecyclerView)
-        var addButton= findViewById<FloatingActionButton>(R.id.fabAdd)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        var addButton = findViewById<FloatingActionButton>(R.id.fabAdd)
         var currentDateTime = LocalDate.now()
         var day = currentDateTime.dayOfMonth
 
+        var dialog = layoutInflater.inflate(R.layout.task_dialog, null)
+        var row_task = layoutInflater.inflate(R.layout.list_row_task, null)
+        var dateTextView = row_task.findViewById<TextView>(R.id.textViewDueDate)
+
 
         val db = Firebase.firestore
-        var taskList= mutableListOf<Task>()
+        var taskList = mutableListOf<Task>()
 
         db.collection("Tasks")
             .get()
-            .addOnSuccessListener { result : QuerySnapshot ->
-                for (document in result){
+            .addOnSuccessListener { result: QuerySnapshot ->
+                for (document in result) {
                     taskList.add(
                         Task(
-                            document.getString("title")!!,document.getString("descrption")!!,document.getString("dueDate")!!,false)
+                            document.id,
+                            document.getString("title")!!,
+                            document.getString("descrption")!!,
+                            document.getString("dueDate")!!,
+                            document.getBoolean("compeleted")!!
+                        )
                     )
 
                 }
-                mRecyclerView.adapter=Task_Adapter(taskList)
+
+                mRecyclerView.adapter = Task_Adapter(taskList)
                 mRecyclerView.adapter?.notifyDataSetChanged()
             }
-
-        mRecyclerView.layoutManager = LinearLayoutManager(this)
-
 
 
 
 
 
         addButton.setOnClickListener {
-            var dialog = layoutInflater.inflate(R.layout.task_dialog,null)
-            var row_task = layoutInflater.inflate(R.layout.list_row_task,null)
-            var dateTextView= row_task.findViewById<TextView>(R.id.textViewDueDate)
-
 
 
             var taskDialog = AlertDialog.Builder(this)
                 .setView(dialog)
                 .show()
 
-            var dateImageView=dialog.findViewById<ImageView>(R.id.imageViewDate)
+            var dateImageView = dialog.findViewById<ImageView>(R.id.imageViewDate)
 
             dateImageView.setOnClickListener {
                 var c = Calendar.getInstance()
@@ -79,12 +80,18 @@ class Home : AppCompatActivity() {
                 var month = c.get(Calendar.MONTH)
                 var day = c.get(Calendar.DAY_OF_MONTH)
 
-                var DatePickerDialog= DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                var DatePickerDialog = DatePickerDialog(
+                    this,
+                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
 
-                    dateTextView.text= "$dayOfMonth/${month+1}/$year"
+                        dateTextView.text = "$dayOfMonth/${month + 1}/$year"
 
 
-                },year,month,day)
+                    },
+                    year,
+                    month,
+                    day
+                )
 
 
                 DatePickerDialog.show()
@@ -92,26 +99,32 @@ class Home : AppCompatActivity() {
             }
 
 
-            var addDialogBtn=dialog.findViewById<Button>(R.id.buttonAdd)
-            var TasktitleEditText=dialog.findViewById<EditText>(R.id.EditTextTaskTitle)
-            var TaskdescrptionEditText= dialog.findViewById<EditText>(R.id.EditTextTaskNote)
+            var addDialogBtn = dialog.findViewById<Button>(R.id.buttonAdd)
+            var TasktitleEditText = dialog.findViewById<EditText>(R.id.EditTextTaskTitle)
+            var TaskdescrptionEditText = dialog.findViewById<EditText>(R.id.EditTextTaskNote)
 
             addDialogBtn.setOnClickListener {
 
-                var title=TasktitleEditText.getText().toString()
-                var desccrption= TaskdescrptionEditText.getText().toString()
-                if(!title.isEmpty()&& !desccrption.isEmpty()){
-                    var task= hashMapOf(
+                var title = TasktitleEditText.getText().toString()
+                var desccrption = TaskdescrptionEditText.getText().toString()
+                if (!title.isEmpty() && !desccrption.isEmpty()) {
+                    var task = hashMapOf(
                         "title" to title,
                         "descrption" to desccrption,
-                        "dueDate" to  dateTextView.text,
+                        "dueDate" to dateTextView.text,
+                        "compeleted" to false
                         //"creationDate" to currentDateTime.toString().toLong()
                     )
 
                     db.collection("Tasks")
                         .add(task)
-                        .addOnSuccessListener { t->
-                            Toast.makeText(this, "Task has been added successfully", Toast.LENGTH_SHORT).show()
+                        .addOnSuccessListener { t ->
+                            Toast.makeText(
+                                this,
+                                "Task has been added successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            mRecyclerView.adapter?.notifyDataSetChanged()
                         }
                         .addOnFailureListener { e ->
                             Log.w(TAG, "Error adding document", e)
@@ -125,5 +138,63 @@ class Home : AppCompatActivity() {
         }
 
 
+    }
 
-    }}
+    override fun onRestart() {
+        super.onRestart()
+        val db = Firebase.firestore
+        var mRecyclerView = findViewById<RecyclerView>(R.id.mRecyclerView)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        var taskList = mutableListOf<Task>()
+
+        db.collection("Tasks")
+            .get()
+            .addOnSuccessListener { result: QuerySnapshot ->
+                for (document in result) {
+                    taskList.add(
+                        Task(
+                            document.id,
+                            document.getString("title")!!,
+                            document.getString("descrption")!!,
+                            document.getString("dueDate")!!,
+                            document.getBoolean("compeleted")!!
+                        )
+                    )
+
+                }
+
+                mRecyclerView.adapter = Task_Adapter(taskList)
+                mRecyclerView.adapter?.notifyDataSetChanged()
+            }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val db = Firebase.firestore
+        var mRecyclerView = findViewById<RecyclerView>(R.id.mRecyclerView)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        var taskList = mutableListOf<Task>()
+
+        db.collection("Tasks")
+            .get()
+            .addOnSuccessListener { result: QuerySnapshot ->
+                for (document in result) {
+                    taskList.add(
+                        Task(
+                            document.id,
+                            document.getString("title")!!,
+                            document.getString("descrption")!!,
+                            document.getString("dueDate")!!,
+                            document.getBoolean("compeleted")!!
+                        )
+                    )
+
+                }
+
+                mRecyclerView.adapter = Task_Adapter(taskList)
+                mRecyclerView.adapter?.notifyDataSetChanged()
+            }
+
+    }
+}
--- app/src/main/java/com/example/todo_app/Task_Adapter.kt
+++ app/src/main/java/com/example/todo_app/Task_Adapter.kt
@@ -1,15 +1,21 @@
 package com.example.todo_app
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
-import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todo_app.model.Task
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Task_Adapter(var data:MutableList<Task>):RecyclerView.Adapter<TaskHolder>() {
+    val db = Firebase.firestore
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
         var v = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_row_task,parent,false)
@@ -20,22 +26,59 @@ class Task_Adapter(var data:MutableList<Task>):RecyclerView.Adapter<TaskHolder>(
     override fun onBindViewHolder(holder: TaskHolder, position: Int) {
         holder.taskTitleTextView.text=data[position].title
         holder.taskNoteTextView.text=data[position].descrption
-        holder.taskDueDateTextView.text=data[position].dueDate.toString()
-        //holder.createDateTextView.text=data[position].createDate.toString()
-//        holder.checkRadio.setOnClickListener {
-//            if(holder.checkRadio.isChecked){
-//
-//                holder.taskTitleTextView.setPaintFlags(holder.taskTitleTextView.getPaintFlags() or
-//                        android.graphics.Paint.STRIKE_THRU_TEXT_FLAG)
-//
-//                holder.taskNoteTextView.setPaintFlags(holder.taskNoteTextView.getPaintFlags() or
-//                        android.graphics.Paint.STRIKE_THRU_TEXT_FLAG)
-//
-//
-//            }
-//     }
+        holder.taskDueDateTextView.text="Due: "+data[position].dueDate.toString()
+
+        if(data[position].stauts){
+            holder.checkState.isChecked
+
+         }else {
+            println(holder.checkState.isChecked.not())
+        }
+
+
+        holder.checkState.setOnClickListener {
+
+
+            if (holder.checkState.isChecked) {
+                db.collection("Tasks")
+                    .document(data[position].id!!)
+                    .update("compeleted",true)
+                holder.taskTitleTextView.setPaintFlags(
+                    holder.taskTitleTextView.getPaintFlags() or
+                            android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+                )
+
+                holder.taskNoteTextView.setPaintFlags(
+                    holder.taskNoteTextView.getPaintFlags() or
+                            android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+                )
+
+            }
+        }
+
+
         holder.updateImage.setOnClickListener {  }
-        holder.deleteCardView.setOnClickListener {  }
+        holder.deleteCardView.setOnClickListener {
+            var deleteDialog=AlertDialog.Builder(holder.deleteCardView.context)
+                .setTitle("Delete")
+                .setMessage("Do you want to delete?")
+                .setPositiveButton("Yes"){dialog, which->
+                    db.collection("Tasks").document(data[position].id!!)
+                        .delete()
+                        .addOnSuccessListener {
+                            Toast.makeText(holder.deleteCardView.context, "Deleted successfully", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(holder.deleteCardView.context, "${it.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    dialog.dismiss()
+                }
+                .setNegativeButton("No"){dialog,which->
+                    dialog.dismiss()
+                }
+                .setIcon(R.drawable.ic_baseline_delete_24)
+                .show()
+        }
 
 
 
@@ -58,6 +101,8 @@ class TaskHolder(v: View): RecyclerView.ViewHolder(v){
     //var checkRadio= v.findViewById<RadioButton>(R.id.radioButton)
     var updateImage= v.findViewById<ImageView>(R.id.imageViewUpdate)
     var deleteCardView= v.findViewById<CardView>(R.id.deleteCardview)
+    var checkState = v.findViewById<CheckBox>(R.id.checkBoxState)
+
 
 
 
