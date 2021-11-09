f00cb36427acc2b7b880c083a96cf6f7a1ce9027
mac <aldagdougfatm@gmail.com>
2021-11-09 14:00:56 +0300

    view after update

--- app/src/main/java/com/example/todo_app/Task_Adapter.kt
+++ app/src/main/java/com/example/todo_app/Task_Adapter.kt
@@ -3,6 +3,7 @@ package com.example.todo_app
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.graphics.Paint
 import android.text.Editable
@@ -13,11 +14,14 @@ import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivities
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.core.content.res.ComplexColorCompat.inflate
 import androidx.core.graphics.drawable.DrawableCompat.inflate
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_app.model.Task
+import com.google.android.material.dialog.MaterialDialogs
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
@@ -178,25 +182,35 @@ class Task_Adapter(var data: MutableList<Task>) : RecyclerView.Adapter<TaskHolde
             var TaskdescrptionEditText = u.findViewById<EditText>(R.id.EditTextTaskNote)
             TaskdescrptionEditText.text=Editable.Factory.getInstance().newEditable(data[position].descrption)
 
+
+            data[position].title=TasktitleEditText.text.toString()
+            data[position].descrption=TaskdescrptionEditText.text.toString()
+            data[position].dueDate=holder.taskDueDateTextView.text.toString()
+            data[position].stauts
             updateDialogBtn.setOnClickListener {
                 db.collection("Tasks")
                     .document(data[position].id!!)
-                    .update(mapOf
+                    .update(
+                        mapOf
                         ("title" to TasktitleEditText.text.toString(),
                         "descrption" to TaskdescrptionEditText.text.toString(),
                         "dueDate" to holder.taskDueDateTextView.text.toString(),
                         "compeleted" to data[position].stauts)
-                    )
+                   )
                     .addOnSuccessListener {
                         Toast.makeText(
                             holder.updateImage.context,
                             "Task has been updated successfully",
                             Toast.LENGTH_SHORT
                         ).show()
-                        var cont=holder.checkState.context
+                        //var cont: Context = Home::class.java as Context
+                        var i=Intent(holder.updateImage.context,Home::class.java)
+                        holder.updateImage.context.startActivity(i)
+
 
                         notifyItemChanged(position)
-                         //notifyDataSetChanged(position)
+                        notifyDataSetChanged( )
+
                     }
                     .addOnFailureListener {e->
                         Log.w(ContentValues.TAG, "Error updating document", e)
