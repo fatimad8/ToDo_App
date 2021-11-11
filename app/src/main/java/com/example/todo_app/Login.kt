ba31f0bc76945ac9fd271be5f5dc9ab8a6ca7726
mac <aldagdougfatm@gmail.com>
2021-11-11 15:51:44 +0300

    login , sign UP

--- app/src/main/java/com/example/todo_app/Login.kt
+++ app/src/main/java/com/example/todo_app/Login.kt
@@ -6,24 +6,53 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var textViewSignUp= findViewById<TextView>(R.id.textViewSignUp)
-        var logEmail=findViewById<EditText>(R.id.EditTextName)
-        var logPass= findViewById<EditText>(R.id.EditTextPass)
-        var loginButton= findViewById<Button>(R.id.buttonLogin)
+        var textViewSignUp = findViewById<TextView>(R.id.textViewSignUp)
+        var logEmail = findViewById<EditText>(R.id.EditTextEmail)
+        var logPass = findViewById<EditText>(R.id.EditTextPass)
+        var loginButton = findViewById<Button>(R.id.buttonLogin)
 
         textViewSignUp.setOnClickListener {
-            var intent= Intent(this,SignUp::class.java)
+            var intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
 
 
 
+        loginButton.setOnClickListener {
 
-     }
-}
+            var auth = Firebase.auth
+
+            auth.signInWithEmailAndPassword(logEmail.text.toString(), logPass.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        var user = auth.currentUser
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                        println("Login Success " + user?.uid)
+                        var intent = Intent(this, Home::class.java)
+                        startActivity(intent)
+                    } else {
+
+                        Toast.makeText(
+                            this,
+                            "Login Failed, You don't have account",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+
+                }
+
+
+        }
+
+
+    }}
--- app/src/main/java/com/example/todo_app/SignUp.kt
+++ app/src/main/java/com/example/todo_app/SignUp.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +17,51 @@ class SignUp : AppCompatActivity() {
 
         var textViewLogin= findViewById<TextView>(R.id.textViewLogin )
         var fname= findViewById<EditText>(R.id.EditTextName)
-        var RegEmail=findViewById<EditText>(R.id.EditTextRegEmail)
-        var RegPass= findViewById<EditText>(R.id.EditTextRegPass)
-        var RegButton= findViewById<Button>(R.id.buttonSignUp)
+        var regEmail=findViewById<EditText>(R.id.EditTextRegEmail)
+        var regPass= findViewById<EditText>(R.id.EditTextRegPass)
+        var regButton= findViewById<Button>(R.id.buttonSignUp)
 
         textViewLogin.setOnClickListener {
             var intent= Intent(this,Login::class.java)
             startActivity(intent)
         }
+
+    regButton.setOnClickListener {
+
+        var auth= Firebase.auth
+
+        auth.createUserWithEmailAndPassword(regEmail.text.toString(),regPass.text.toString())
+            .addOnCompleteListener { task->
+
+                if(task.isSuccessful){
+
+                    println("User has been registered successfully with UID "+ auth.currentUser?.uid)
+
+                    val u = hashMapOf(
+                        "email" to auth.currentUser?.email,
+                        "firstname" to fname.text.toString()
+
+                    )
+
+                    var db= Firebase.firestore
+
+                    db.collection("users").document(auth.currentUser?.uid.toString())
+                        .set(u)
+                    var intent = Intent(this, Login::class.java)
+                    startActivity(intent)
+
+                }else{
+
+                    println("Error")
+                }
+            }.addOnFailureListener {
+                println(it.message)
+            }
     }
+
+
+
+    }
+
+
 }
